881dce318386b76df175c76cdbe2e7b97bc7ef23
azmint <az0.mint9@gmail.com>
2018-10-05 21:17:58 +0900

    プロジェクト ファイルを追加します。

---
+++ TaskManagement/Data/User.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagement
+{
+    class User
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+
+        public override string ToString()
+        {
+            return "User[" + id + "," + name + "]";
+        }
+    }
+}
---
+++ TaskManagement/Support/Json/Json.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace TaskManagement.Support.Json
+{
+    class Json
+    {
+        private string Text { get; }
+
+        private Json(string text)
+        {
+            this.Text = text ?? throw new ArgumentNullException(nameof(text));
+        }
+
+        public static Json From(string url)
+        {
+            var hc = new HttpClient();
+            var res = hc.GetAsync(url);
+            var text = res.Result.Content.ReadAsStringAsync().Result;
+            return new Json(text);
+        }
+
+        public T As<T>()
+        {
+            var js = new JsonSerializer();
+            var jr = new JsonTextReader(new StringReader(this.Text));
+            return JsonConvert.DeserializeObject<T>(this.Text);
+        }
+
+        public List<T> AsList<T>()
+        {
+            var js = new JsonSerializer();
+            var jr = new JsonTextReader(new StringReader(this.Text));
+            return JsonConvert.DeserializeObject<List<T>>(this.Text);
+        }
+    }
+}
